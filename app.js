94c4b714b052a3cf23b020392e35f6874f29cf37
Pranjal Rai <42915990+pranjalrai-iitd@users.noreply.github.com>
2021-01-15 15:21:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,35 @@ window.onload = function() {
     const navslide = () => {
         const burger = document.querySelector('.burger');
         const nav = document.querySelector('.nav-links');
-
+        const head = document.querySelector('header');
+        var scrollTop = $(window).scrollTop();
+        elementOffset = $(".welcome").offset().top;
+        distance = (elementOffset - scrollTop);
+        console.log(distance);
         burger.addEventListener('click', () => {
             nav.classList.toggle('nav-links-active');
+            if (distance > -20) {
+                head.classList.toggle('header-active');
+            }
+            console.log("Hi");
         });
     }
-
     navslide();
+}
 
+window.onscroll = function() {
+    const scroll = () => {
+        const head = document.querySelector('header');
+        var scrollTop = $(window).scrollTop();
+        elementOffset = $(".welcome").offset().top;
+        distance = (elementOffset - scrollTop);
+        if (distance < -20) {
+            head.classList.add('header-active');
+        }
+        if (distance > -20) {
+            head.classList.remove('header-active');
+        }
+
+    }
+    scroll();
 }
